2405cbe5d492f26ecbe52f9ecbeae69dd7ce7450
FiOS-repo <fibart@gmx.de>
2024-11-24 14:20:24 +0100

    added: root checking

--- scaffhold.sh
+++ scaffhold.sh
@@ -1,6 +1,6 @@
 #Configuration
 PACKAGE_MANAGER="apt"
-SUDO=true
+ROOT=true
 DEPENDENCIES=("curl", "wget", "zip", "unzip", "tar")
 SCRIPTS='echo Hello World'
 UPDATE_PACKAGES=true
@@ -13,3 +13,17 @@ echo "
 ██║     ██║██║ ╚████║██║ ╚████║███████║    ███████║╚██████╗██║  ██║██║██║        ██║   ███████║
 ╚═╝     ╚═╝╚═╝  ╚═══╝╚═╝  ╚═══╝╚══════╝    ╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝        ╚═╝   ╚══════╝
 "
+
+main () {
+    if [ $ROOT = true ];
+    then
+        if [ $USER != "root" ];
+        then 
+            echo "Please run this script as root"
+            exit 1
+        fi
+    fi
+    echo "Running script as user: $USER"
+}
+
+main
